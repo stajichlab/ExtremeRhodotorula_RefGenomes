81036c0bd6a74247652fccb4a5f44e30f83c1022
Jason Stajich <jasonstajich.phd@gmail.com>
2022-10-19 13:21:33 -0700

    updates for new runs and steps

---
+++ pipeline/asm/01_canu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash -l
+#SBATCH -p short --out logs/launch_canu.%a.log -a 1-4
+module load canu
+IN=input/nanopore
+OUT=asm/canu
+N=${SLURM_ARRAY_TASK_ID}
+if [ -z $N ]; then
+    N=$1
+    if [ -z $N ]; then
+	echo "no value for SLURM ARRAY - specify with -a or cmdline"
+    fi
+fi
+
+mkdir -p $OUT
+IFS=,
+SAMPLES=samples.csv
+
+sed -n ${N}p $SAMPLES | while read STRAIN NANOPORE SUBPHYLUM PHYLUM
+do
+	canu -p ${STRAIN} -d $OUT/${STRAIN} genomeSize=20m -nanopore-raw $IN/$NANOPORE.fq.gz
+done
---
+++ pipeline/asm/01_flye.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -C xeon -N 1 -c 24 --mem 96gb --out logs/flye.%a.log -a 1-4
+module load Flye
+IN=input/nanopore
+OUT=asm/flye
+hostname
+CPU=$SLURM_CPUS_ON_NODE
+if [ -z $CPU ]; then
+	CPU=2
+fi
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ -z $N ]; then
+    N=$1
+    if [ -z $N ]; then
+        echo "Need an array id or cmdline val for the job"
+        exit
+    fi
+fi
+
+
+mkdir -p $OUT
+IFS=,
+SAMPLES=samples.csv
+
+sed -n ${N}p $SAMPLES | while read STRAIN NANOPORE SUBPHYLUM PHYLUM
+do
+	flye --nano-hq $IN/$NANOPORE.fq.gz --out-dir $OUT/$STRAIN --threads $CPU --scaffold
+done
