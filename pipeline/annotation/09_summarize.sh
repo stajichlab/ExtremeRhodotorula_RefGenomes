234925432436f8e5553e51eda47fabe6565632fb
Jason Stajich <jasonstajich.phd@gmail.com>
2022-11-03 16:06:07 -0700

    notice

--- pipeline/annotation/09_summarize.sh
+++ pipeline/annotation/09_summarize.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -l
 
 #SBATCH -p short -N 1 -n 2 --mem 8gb
 
-perl scripts/summarize_annotation.pl > annotation_summary.tsv
+echo "would do summary of annotation - but a lot of this is generated in .json files from funanotate - this was an attempt to aggregate all of these in a single table"
+echo "still need to re-write that bit"
+#perl scripts/summarize_annotation.pl > annotation_summary.tsv
+
